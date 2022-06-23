ba561e724f8d4d86e8d379f8ca0fdbcaa1b0ec9e
firstaroyand <firstaroyan09@gmail.com>
2022-06-23 10:17:19 +0700

    docs(storybook): add 'input' storybook

---
+++ packages/components/src/ui-components/form/__stories__/InputSelect/InputSelect.stories.tsx
@@ -0,0 +1,216 @@
+import { Meta, Story } from '@storybook/react';
+import * as React from 'react';
+import { InputGroup, InputSelect, InputSelectProps, FormLabel, InputPrefix } from '../../components';
+import { Stack } from '../../../../layout';
+import { Avatar } from '../../../avatar';
+
+export default {
+  title: 'Core/Components/Form/InputSelect',
+  component: InputSelect,
+  argTypes: {
+    placeholder: {
+      control: 'text',
+    },
+    errors: {
+      control: 'boolean',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+    size: {
+      options: ['md', 'lg'],
+    },
+    label: {
+      control: 'text',
+    },
+    width: {
+      control: 'number',
+    },
+    maxHeight: {
+      control: 'number',
+    },
+  },
+} as Meta<InputSelectProps<ValueSchema>>;
+
+interface ValueSchema {
+  label: string;
+  value: string;
+}
+
+export const InputSelectExample: Story<InputSelectProps<ValueSchema>> = ({
+  placeholder,
+  disabled,
+  size,
+  label,
+  errors,
+  width,
+  maxHeight,
+}) => {
+  const [selected, setSelected] = React.useState<ValueSchema | null>(null);
+  const [items] = React.useState([
+    { label: 'ab', value: 'a' },
+    { label: 'abc', value: 'b' },
+    { label: 'bcd', value: 'c' },
+    { label: 'cde', value: 'd' },
+    { label: 'def', value: 'e' },
+    { label: 'efg', value: 'f' },
+    { label: 'fgh', value: 'g' },
+    { label: 'ghi', value: 'h' },
+    { label: 'hij', value: 'i' },
+    { label: 'ijk', value: 'j' },
+    { label: 'jkl', value: 'k' },
+  ]);
+  return (
+    <InputSelect
+      label={label}
+      size={size}
+      selectedItem={selected}
+      placeholder={placeholder}
+      disabled={disabled}
+      errors={errors}
+      itemToString={item => (item ? `${item.label}` : '')}
+      itemValue={item => (item ? `${item.label}` : '')}
+      handleSelectedItemChange={({ selectedItem }) => {
+        if (selectedItem) {
+          setSelected(selectedItem);
+        }
+      }}
+      items={items}
+      width={width}
+      maxHeight={maxHeight}
+    />
+  );
+};
+InputSelectExample.args = {
+  placeholder: 'Select...',
+  size: 'md',
+  disabled: false,
+  label: 'Label input',
+  errors: false,
+  width: '100%',
+  maxHeight: 250,
+};
+
+export const InputSelectWithPrefix: Story<InputSelectProps<ValueSchema>> = ({
+  placeholder,
+  disabled,
+  size,
+  errors,
+  width,
+  maxHeight,
+}) => {
+  const [selected, setSelected] = React.useState<ValueSchema | null>(null);
+  const [items] = React.useState([
+    { label: 'ab', value: 'a' },
+    { label: 'abc', value: 'b' },
+    { label: 'bcd', value: 'c' },
+    { label: 'cde', value: 'd' },
+    { label: 'def', value: 'e' },
+    { label: 'efg', value: 'f' },
+    { label: 'fgh', value: 'g' },
+    { label: 'ghi', value: 'h' },
+    { label: 'hij', value: 'i' },
+    { label: 'ijk', value: 'j' },
+    { label: 'jkl', value: 'k' },
+  ]);
+  return (
+    <Stack spacing="xs">
+      <FormLabel htmlFor="textDummy">Select input</FormLabel>
+      <InputGroup width="100%" maxWidth={360}>
+        <InputPrefix disabled={disabled} zIndex={1}>
+          Sort by
+        </InputPrefix>
+        <InputSelect
+          size={size}
+          selectedItem={selected}
+          placeholder={placeholder}
+          disabled={disabled}
+          errors={errors}
+          itemToString={item => (item ? `${item.label}` : '')}
+          itemValue={item => (item ? `${item.label}` : '')}
+          handleSelectedItemChange={({ selectedItem }) => {
+            if (selectedItem) {
+              setSelected(selectedItem);
+            }
+          }}
+          items={items}
+          width={width}
+          maxHeight={maxHeight}
+          inputStyle={{
+            pl: 58,
+          }}
+        />
+      </InputGroup>
+    </Stack>
+  );
+};
+InputSelectWithPrefix.args = {
+  placeholder: 'Select...',
+  size: 'md',
+  disabled: false,
+  errors: false,
+  width: '100%',
+  maxHeight: 250,
+};
+
+export const InputSelectWithPrefixAvatar: Story<InputSelectProps<ValueSchema>> = ({
+  placeholder,
+  disabled,
+  size,
+  errors,
+  width,
+  maxHeight,
+}) => {
+  const [selected, setSelected] = React.useState<ValueSchema | null>(null);
+  const [items] = React.useState([
+    { label: 'Ai Nakamura', value: 'a' },
+    { label: 'Eren Yeager', value: 'b' },
+    { label: 'Rahmat Sentosa', value: 'c' },
+    { label: 'Goerge Frex', value: 'd' },
+    { label: 'Zeni Luxfriss', value: 'e' },
+  ]);
+  return (
+    <Stack spacing="xs">
+      <FormLabel htmlFor="textDummy">Select input</FormLabel>
+      <InputGroup width="100%" maxWidth={360}>
+        {selected?.label && (
+          <InputPrefix disabled={disabled} zIndex={1}>
+            <Avatar name={selected?.label} size="sm" />
+          </InputPrefix>
+        )}
+        <InputSelect
+          size={size}
+          selectedItem={selected}
+          placeholder={placeholder}
+          disabled={disabled}
+          errors={errors}
+          itemToString={item => (item ? `${item.label}` : '')}
+          itemValue={item => (item ? `${item.label}` : '')}
+          handleSelectedItemChange={({ selectedItem }) => {
+            if (selectedItem) {
+              setSelected(selectedItem);
+            }
+          }}
+          items={items}
+          width={width}
+          maxHeight={maxHeight}
+          inputStyle={
+            selected?.label
+              ? {
+                  pl: 44,
+                }
+              : {}
+          }
+        />
+      </InputGroup>
+    </Stack>
+  );
+};
+InputSelectWithPrefixAvatar.args = {
+  placeholder: 'Select...',
+  size: 'lg',
+  disabled: false,
+  errors: false,
+  width: '100%',
+  maxHeight: 250,
+};
---
+++ packages/components/src/ui-components/form/__stories__/InputText.stories.tsx
@@ -0,0 +1,100 @@
+import { Meta, Story } from '@storybook/react';
+import { MdLock, MdWarning } from "react-icons/md";
+import * as React from 'react';
+
+import { Stack } from '../../../layout';
+import { InputGroup, InputIcon, FormLabel, InputPrefix, InputSuffix } from '../components';
+import { InputText, InputTextProps } from '../components/InputText';
+
+export default {
+  title: 'Core/Components/Form/InputText',
+  component: InputText,
+  argTypes: {
+    errors: {
+      control: 'boolean',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+    inputSize: {
+      options: ['md', 'lg'],
+    },
+  },
+} as Meta<InputTextProps>;
+
+export const Example: Story<InputTextProps> = ({ errors, inputSize, disabled }) => {
+  return (
+    <Stack spacing="xs">
+      <FormLabel htmlFor="textDummy">Text input</FormLabel>
+      <InputText
+        id="textDummy"
+        name="textDummy"
+        placeholder="Type here..."
+        errors={errors}
+        inputSize={inputSize}
+        disabled={disabled}
+      />
+    </Stack>
+  );
+};
+Example.args = {
+  errors: false,
+  disabled: false,
+  inputSize: 'md',
+};
+
+export const WithIcons: Story<InputTextProps> = ({ errors, inputSize, disabled }) => {
+  return (
+    <Stack spacing="xs">
+      <FormLabel htmlFor="textDummy">Text input</FormLabel>
+      <InputGroup width="100%" maxWidth={360}>
+        <InputIcon icon={MdLock} iconPosition="left" iconText="Username" disabled={disabled} />
+        <InputText
+          id="textDummy"
+          name="textDummy"
+          placeholder="Type here..."
+          errors={errors}
+          inputSize={inputSize}
+          disabled={disabled}
+          width="100%"
+          pl={36}
+          pr={36}
+        />
+        <InputIcon icon={MdWarning} iconPosition="right" iconText="Username" disabled={disabled} />
+      </InputGroup>
+    </Stack>
+  );
+};
+WithIcons.args = {
+  errors: false,
+  disabled: false,
+  inputSize: 'md',
+};
+
+export const WithPrefixSuffix: Story<InputTextProps> = ({ errors, inputSize, disabled }) => {
+  return (
+    <Stack spacing="xs">
+      <FormLabel htmlFor="textDummy">Text input</FormLabel>
+      <InputGroup width="100%" maxWidth={360}>
+        <InputPrefix disabled={disabled}>Prefix</InputPrefix>
+        <InputText
+          id="textDummy"
+          name="textDummy"
+          placeholder="Type here..."
+          errors={errors}
+          inputSize={inputSize}
+          disabled={disabled}
+          width="100%"
+          pl={49}
+          pr={49}
+        />
+        <InputSuffix disabled={disabled}>Suffix</InputSuffix>
+      </InputGroup>
+    </Stack>
+  );
+};
+WithIcons.args = {
+  errors: false,
+  disabled: false,
+  inputSize: 'md',
+};
